efcefd617cf3973d5c819f66f1fac5af983b3130
hoinguyenpr <hoinguyenpr.97@gmail.com>
2021-04-25 18:38:16 +0700

    a

---
+++ short-training/selection.rb
@@ -0,0 +1,12 @@
+def if_statement
+    x = 10
+    if x > 2
+        puts "if: x is greater than 2"
+    elsif x <= 2 and x!=0
+        puts "if: x is 1"
+    else
+        puts "if: I can't guess the number"
+    end
+end
+
+if_statement
